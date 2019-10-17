7c4f66d2578f5b48cb76e5695346df0ee496f506
yangyongjie <437974912@qq.com>
2019-10-17 16:06:47 +0800

    add project

---
+++ ExchangeRateCalculator/ExchangeRateCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExchangeRateCalculator
+//
+//  Created by 杨永杰 on 2019/10/17.
+//  Copyright © 2019 YYongJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ExchangeRateCalculator/ExchangeRateCalculator/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ExchangeRateCalculator
+//
+//  Created by 杨永杰 on 2019/10/17.
+//  Copyright © 2019 YYongJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
